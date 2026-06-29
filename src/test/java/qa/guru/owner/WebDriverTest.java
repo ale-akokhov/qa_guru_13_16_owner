61b9b0b41b0fffefd672c1f64a3d52001b478f27
Alexander <ale-akokhov@ayndex.ru>
2026-06-29 21:23:23 +0300

    Tests added

--- src/test/java/qa/guru/owner/WebDriverTest.java
+++ src/test/java/qa/guru/owner/WebDriverTest.java
@@ -1,4 +1,37 @@
 package qa.guru.owner;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WebDriverTest {
+
+    @Test
+    public void testGithub0() {
+        WebDriver driver = getDriver();
+        driver.get("https://github.com/");
+        String title = driver.getTitle();
+        assertEquals("GitHub · Change is constant. GitHub keeps you ahead. · GitHub", title);
+        driver.quit();
+
+    }
+
+    @Test
+    public void testGithub1() {
+        WebDriver driver = getDriver();
+        driver.get("https://github.com/");
+        String title = driver.getTitle();
+        assertEquals("GitHub · Change is constant. GitHub keeps you ahead. · GitHub", title);
+        driver.quit();
+
+    }
+
+    private WebDriver getDriver() {
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }
 }
